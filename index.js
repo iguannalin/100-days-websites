aa8dcbb7f4d6f3eb926da3b08dd3dac88fdb275f
seasons986 <annalin12345@gmail.com>
2023-07-30 13:58:33 -0400

    update

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ const dt = new Date();
 const daysSince = Math.round((dt - d0)/86400000);
 
 window.addEventListener("load", () => {
+  const main = document.getElementById("main");
   let sites;
   fetch("./items.json").then((d) => d.json()).then((r) => {
     sites = r;
     for (let i = 1; i <= daysSince; i++) {
       if (!sites[i]) return;
-      console.log(sites[i]);
       let elem = document.createElement('div');
       let anchor = document.createElement('a');
       elem.className = "site";
       anchor.href = `https://annaylin.com/100-days/${sites[i].name.replace(" ", "")}`;
       anchor.innerHTML = `day ${i} — ${sites[i].name}`;
       elem.appendChild(anchor);
-      document.body.appendChild(elem);
+      main.appendChild(elem);
     }
   });
 });
