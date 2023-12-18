57590a39082fde99552386a628468058780ae38f
seasons986 <annalin12345@gmail.com>
2023-12-17 20:09:50 -0500

    update

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const daysSince = Math.round((dt - d0)/86400000);
 window.addEventListener("load", () => {
   const main = document.getElementById("main");
   let sites;
-  let bolded = [
-    "https://annaylin.com/100-days/directions",
-    "https://annaylin.com/100-days/bracketsclock/",
-    "https://annaylin.com/100-days/sunmoonsky/",
-    "https://annaylin.com/100-days/transpilation/",
-    "https://annaylin.com/100-days/ctw/2150",
-    "https://annaylin.com/100-days/ctw/how-to",
-    "https://annaylin.com/100-days/elements",
-    "https://annaylin.com/100-days/ctw/progress",
-    "https://annaylin.com/100-days/buxing",
-    "https://annaylin.com/100-days/grass",
-    "https://annaylin.com/100-days/san"
+  let favorites = [
+    "directions",
+    "bracketsclock",
+    "sunmoonsky",
+    "transpilation",
+    "ctw/2150",
+    "ctw/how-to",
+    "elements",
+    "ctw/progress",
+    "buxing",
+    "grass",
+    "sand"
   ];
   fetch("./items.json").then((d) => d.json()).then((r) => {
     sites = r;
@@ -26,10 +26,10 @@ window.addEventListener("load", () => {
       let anchor = document.createElement('a');
       elem.className = "site";
       anchor.href = `https://annaylin.com/100-days/${sites[i].name.replace(" ", "")}`;
-      if (bolded.includes(anchor.href)) {
+      if (favorites.includes(sites[i].name)) {
         anchor.style.fontStyle = "italic";
         anchor.style.fontWeight = "800";
-        anchor.innerHTML += "*";
+        anchor.innerHTML += "* ";
       }
       anchor.innerHTML += `day ${i} — ${sites[i].name}`;
       elem.appendChild(anchor);
